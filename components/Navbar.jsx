59f73d6457b2f19631a7ca7e37385a4a62ae1c9e
Foyinsola Tony-Aitanun <122688011+desireetony@users.noreply.github.com>
2024-09-13 15:18:58 +0100

    Initial commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,141 @@
+import React, { useState } from 'react';
+import {
+  FaBars,
+  FaTimes,
+  FaGithub,
+  FaLinkedin,
+  FaFacebook,
+  FaLinkedinIn,
+} from 'react-icons/fa';
+import { HiOutlineMail } from 'react-icons/hi';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+
+import { Link } from 'react-scroll';
+import name_logo2 from '../assets/name_logo2.png'
+
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
+
+  return (
+    <div className='fixed w-full h-[80px] flex justify-between items-center p-10 bg-[#0a192f] text-gray-300'>
+      <div>
+        <img src={name_logo2} alt='Logo Image' style={{ width: '150px' }} />
+      </div>
+
+      {/* menu */}
+      <ul className='hidden md:flex '>
+        <li className='hover:border-b-4  border-rose-600'>
+          <Link to='home' smooth={true} duration={500} >
+            Home
+          </Link>
+        </li>
+        <li className='hover:border-b-4  border-rose-600'>
+          <Link to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className='hover:border-b-4  border-rose-600'>
+          <Link to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className='hover:border-b-4  border-rose-600'>
+          <Link to='work' smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className='hover:border-b-4  border-rose-600'>
+          <Link to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      {/* Hamburger */}
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
+
+      {/* Mobile menu */}
+      <ul
+        className={
+          !nav
+            ? 'hidden'
+            : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
+        }
+      >
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='work' smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
+
+      {/* Social icons */}
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='https://www.linkedin.com/in/foyinsola-tony-aitanun-833aa2253/?lipi=urn%3Ali%3Apage%3Ad_flagship3_feed%3BkT%2FND%2FqgQNGZKeapKga9aQ%3D%3D'
+            >
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='https://github.com/desireetony'
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href="mailto:foyinsolatony@gmail.com?subject=Inquiry&body=Hello%20there,"
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/FOYINSOLA_TONY-AITANUN _SE_CV.pdf'
+              download
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ data/data.js
@@ -0,0 +1,47 @@
+
+import RealEst from '../assets/projects/realestate.jpg'
+import react_crypto from '../assets/react_crypto.png'
+import qr_code_generator from '../assets/qr_code_generator.png'
+import burger_house from '../assets/burger_house.png'
+import booking from '../assets/booking.com.png'
+import chatbot from '../assets/chatbot.png'
+
+export const data=[
+    {
+        id:1,
+        name:"QR Code Generator",
+        image:qr_code_generator,
+        github:"https://github.com/desireetony/Qr-gen.git",
+        live:"",
+    },
+    {
+        id:2,
+        name:"Chatbot",
+        image:chatbot,
+        github:"https://github.com/desireetony/Chatbot.git",
+        live:"",
+    },
+    {
+        id:3,
+        name:"Burger House Website",
+        image:burger_house,
+        github:"https://github.com/desireetony/Burger_house.git",
+        live:"",
+    },
+    {
+        id:4,
+        name:"Booking.com",
+        image:booking,
+        github:"https://github.com/desireetony/booking_land.git",
+        live:"",
+    },
+    {
+        id:5,
+        name:"Crypto Market Cap Page",
+        image:react_crypto,
+        github:"https://github.com/desireetony/cryptorical.git",
+        live:"",
+    },
+
+
+]
